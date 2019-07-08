32d9bbd718147a90bd82f911e0af7ab5b49331e7
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-07-08 08:18:24 -0500

    creating title row, actions row and description text

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -10,8 +10,22 @@ class BasicPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),),
-          Container(
+          _createImage(),
+          _createTitleRow(),
+          _createActionsRow(),
+          _createDescriptionText()
+        ],
+      )
+    );
+  }
+
+
+  Widget _createImage() {
+    return Image(image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),);
+  }
+
+  Widget _createTitleRow() {
+    return Container(
             padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
             child: Row(
               children: <Widget>[
@@ -19,9 +33,9 @@ class BasicPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text("title", style: this.titleStyle),
+                      Text("Beautiful bridge and trees", style: this.titleStyle),
                       SizedBox(height: 7.0,),
-                      Text("subtitle", style: this.subtitleStyle),
+                      Text("Stuttgart, Deutschland", style: this.subtitleStyle),
                     ],
                   ),
                 ),
@@ -29,9 +43,38 @@ class BasicPage extends StatelessWidget {
                 Text("41", style: TextStyle(fontSize: 20.0))
               ],
             ),
-          )
-        ],
-      )
+          );
+  }
+
+  Widget _createActionsRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _createActionTile("Call", Icons.call),
+        _createActionTile("Route", Icons.near_me),
+        _createActionTile("Share", Icons.share)
+      ],
+    );
+  }
+
+  Widget _createActionTile(String title, IconData iconData) {
+    return Column(
+          children: <Widget>[
+            Icon(iconData, color: Colors.blue, size: 40.0,),
+            SizedBox(height: 5.0,),
+            Text(title, style: TextStyle(fontSize: 15.0, color: Colors.blue),)
+          ],
+        );
+  }
+
+
+  Widget _createDescriptionText() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Lorem veniam consectetur enim adipisicing magna cupidatat in occaecat aliqua dolor. Id esse quis pariatur laborum ad duis voluptate ea labore est nostrud. Tempor ad pariatur nulla cillum ipsum do id culpa non exercitation esse ullamco.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
