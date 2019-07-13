9ff8a428e8e8d4be3c9a62c75dd9cf96f1299d08
Daniel Reyes Sanchez <dreyes.developer@gmail.com>
2019-07-13 06:46:17 -0500

    Working with tiles page. Gradients and views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_training/src/pages/basic_page.dart';
 import 'package:flutter_ui_training/src/pages/scroll_page.dart';
+import 'package:flutter_ui_training/src/pages/tiles_page.dart';
 
 
 void main() => runApp(MyApp());
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UI Training',
-      initialRoute: 'scroll',
+      initialRoute: 'tiles',
       routes: { 
         'basic': (BuildContext context) => BasicPage(),
-        'scroll': (BuildContext context) => ScrollPage()
+        'scroll': (BuildContext context) => ScrollPage(),
+        'tiles': (BuildContext context) => TilesPage(),
       },
     );
   }
